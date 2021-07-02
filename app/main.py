5ee180b7de5a4f955c86547f67b507a344913494
Rinav Kasthuri <rioton2k17@outlook.com>
2021-07-02 04:12:40 +0000

    ready for production deployment

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ from utils import get_base_url, allowed_file, and_syntax
     port may need to be changed if there are multiple flask servers running on same server
     comment out below three lines of code when ready for production deployment
 '''
-# port = 12356
+# port = 12345
 # base_url = get_base_url(port)
 # app = Flask(__name__, static_url_path=base_url+'static')
 
@@ -50,7 +50,7 @@ def BTS():
     return render_template('BehindThe-Scene.html')
 
 @app.route('/About-the-Team.html')
-#@app.route(base_url + '/About-the-Team.html')
+# @app.route(base_url + '/About-the-Team.html')
 def team():
     return render_template('About-the-Team.html')
 
@@ -61,14 +61,16 @@ def team():
 def home_post(filename=None):
     # check if the post request has the file part
     if 'file' not in request.files:
-        flash('No file part')
+#         flash('No file part')
         return redirect(request.url)
 
     file = request.files['file']
-    # if user does not select file, browser also
-    # submit an empty part without filename
-    if file.filename == '':
-        flash('No selected file')
+
+    # if user does not select file or selects an invalid file,
+    # browser also submit an empty part without filename
+    if file.filename in [".png", ".jpg", ".jpeg"] or file.filename == "" \
+        or file.filename.split(".") in [".tiff", ".gif"]:
+#         flash('No selected file')
         return redirect(request.url)
 
     if file and allowed_file(file.filename):
